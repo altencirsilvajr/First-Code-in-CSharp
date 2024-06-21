a4572b6952669add7a860319b63264d32a609e06
Altencir <altencirsilvajr@gmail.com>
2024-06-21 10:35:28 -0300

    First commit in C#. Woooohuuuu!

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! This is my first code in C#. My name is Altencir Junior. Let's go learn together? See you soon...");
