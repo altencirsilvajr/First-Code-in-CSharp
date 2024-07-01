293362e17d1a097275332635e7781c7e110d930e
Altencir <altencirsilvajr@gmail.com>
2024-07-01 20:46:10 -0300

    add exercise b, on exercise 13

--- Program_13.cs
+++ Program_13.cs
@@ -1,13 +1,25 @@
 ﻿// / Exercício 13)
 
-double a,b,c,soma,dif ;
-a = 2;
-b = 3;
-c = 4;
-dif = -5;
-soma = a + b + c - dif ;
-dif = Math.Pow (soma,2);
-soma = Math.Sqrt (dif);
-a = c;
-b = -3 ;
-Console . WriteLine ($"O resultado será: {a},{b},{c},{ soma } e { dif }");
+
+// double a,b,c,soma,dif;
+// a = 2;
+// b = 3;
+// c = 4;
+// dif = -5;
+// soma = a + b + c - dif ;
+// dif = Math.Pow (soma,2);
+// soma = Math.Sqrt (dif);
+// a = c;
+// b = -3 ;
+// Console . WriteLine ($"O resultado será: {a},{b},{c},{ soma } e { dif }");
+
+// int x, y, resto, quociente;
+// x = 25;
+// y = 52;
+// resto = y % x;
+// quociente = y / x;
+// x = resto;
+// y = quociente;
+// resto = x % y;
+// quociente = x / y;
+// Console.WriteLine($"resto {resto}\nquociente {quociente}");
